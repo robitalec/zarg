0b7f560b52c85b7e29e3cee75bb54db304b8040a
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-14 13:20:34 -0700

    substitute priors, return raw target

--- R/zar_brms_priors.R
+++ R/zar_brms_priors.R
@@ -1,3 +1,10 @@
-zar_brms_priors <- function() {
+zar_brms_priors <- function(name, priors) {
+  command <- substitute(priors)
   
+  return(
+    targets::tar_target_raw(
+    name,
+    command
+    )
+  )
 }
