7bf8a2af3ef20b3743dd11b20b2ea3726c6503d1
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-14 17:05:19 -0700

    mv commands, names back to zar_brms

--- R/zar_brms.R
+++ R/zar_brms.R
@@ -36,18 +36,142 @@ zar_brms <-
            cue = targets::tar_option_get("cue")
   ) {
   name_deparse <- deparse(substitute(name))
-  data_deparse <- deparse(substitute(data))
   
   name_formula <- paste(name_deparse, 'formula', sep = '_')
   name_priors <- paste(name_deparse, 'priors', sep = '_')
+  name_data <- paste(name_deparse, 'data', sep = '_')
   name_sample_priors <- paste(name_deparse, 'sample_priors', sep = '_')
   name_stancode <- paste(name_deparse, 'stancode', sep = '_')
   name_sample <- paste(name_deparse, 'sample', sep = '_')
   
-  c(
-    zar_brms_formula(name_formula, formula),
-    zar_brms_priors(name_priors, priors),
-    zar_brms_sample_priors(name_sample_priors, formula, priors, data, chains, 
-                           iter, cores, save_model),
+  
+  command_formula <- substitute(brms::brmsformula(formula))
+  command_priors <- substitute(priors)
+  command_data <- substitute(data)
+  command_sample_priors <-  substitute(
+    brms::brm(
+      formula = formula,
+      data = data,
+      prior = priors,
+      sample_prior = 'only',
+      chains = chains,
+      iter = iter,
+      cores = cores,
+      save_model = save_model
+    ),
+    env = list(
+      formula = as.symbol(name_formula),
+      priors = as.symbol(name_priors),
+      data = as.symbol(name_data)
+    )
+  )
+  command_stancode <- substitute(
+    make_stancode(
+      formula = formula,
+      data = data,
+      prior = priors,
+      family = if(is.null(family)) gaussian() else family
+    ),
+    env = list(
+      formula = as.symbol(name_formula),
+      priors = as.symbol(name_priors),
+      data = as.symbol(name_data)
+    ))
+  
+  target_formula <- tar_target_raw(
+    name = name_formula,
+    command = command_formula,
+    packages = packages,
+    library = library,
+    format = format,
+    error = error,
+    memory = memory,
+    garbage_collection = garbage_collection,
+    deployment = deployment,
+    priority = priority,
+    cue = cue
+  )
+  target_priors <- tar_target_raw(
+    name = name_priors,
+    command = command_priors,
+    packages = packages,
+    library = library,
+    format = format,
+    error = error,
+    memory = memory,
+    garbage_collection = garbage_collection,
+    deployment = deployment,
+    priority = priority,
+    cue = cue
+  )
+  target_data <- tar_target_raw(
+    name = name_data,
+    command = command_data,
+    packages = packages,
+    library = library,
+    format = format,
+    error = error,
+    memory = memory,
+    garbage_collection = garbage_collection,
+    deployment = deployment,
+    priority = priority,
+    cue = cue
+  )
+  target_sample_priors <- tar_target_raw(
+    name = name_sample_priors,
+    command = command_sample_priors,
+    packages = packages,
+    library = library,
+    format = format,
+    error = error,
+    memory = memory,
+    garbage_collection = garbage_collection,
+    deployment = deployment,
+    priority = priority,
+    cue = cue
+  )
+  target_stancode <- tar_target_raw(
+    name = name_stancode,
+    command = command_stancode,
+    packages = packages,
+    library = library,
+    format = format,
+    error = error,
+    memory = memory,
+    garbage_collection = garbage_collection,
+    deployment = deployment,
+    priority = priority,
+    cue = cue
+  )
+  
+  
+  
+  
+  
+  
+  return(
+    list(
+      target_formula,
+      target_priors,
+      target_data,
+      target_sample_priors,
+      target_stancode
+    )
+  )
+  
+  
+  # name_priors <- paste(name_deparse, 'priors', sep = '_')
+  # name_sample_priors <- paste(name_deparse, 'sample_priors', sep = '_')
+  # name_stancode <- paste(name_deparse, 'stancode', sep = '_')
+  # name_sample <- paste(name_deparse, 'sample', sep = '_')
+  
+  # symbol_formula <- as.symbol(name_formula)
+  
+  # c(
+    # zar_brms_formula(name_formula, substitute(formula))#,
+    # zar_brms_priors(name_priors, priors),
+    # zar_brms_sample_priors(name_sample_priors, symbol_formula, priors, data, chains, 
+    #                        iter, cores, save_model)
+  # )
   
 }
