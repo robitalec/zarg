31e656f86cc4902c54ec019ac2065100522e6960
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-08 10:58:45 -0400

    fst command

--- tests/testthat/test-zar-brms.R
+++ tests/testthat/test-zar-brms.R
@@ -1,2 +1,16 @@
 context('test-zar_brms')
 
+data_name <- 'mtcars'
+zb <- zar_brms(
+  mtcars,
+  formula = mpg ~ hp,
+  prior = c(prior(normal(-0.5, 0.2), class = 'b')),
+  family = gaussian(),
+  data = cars_data,
+  chains = 4,
+  iter = 2000,
+  cores = 1,
+  # control = 0.8,
+  save_model = NULL
+)
+
